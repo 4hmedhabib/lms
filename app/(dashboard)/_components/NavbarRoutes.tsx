3cb166184155a8e3413f3b79e3f686e35fb31b85
4hmedhabib <info.ahmed.developer@gmail.com>
2024-07-29 21:09:37 +0300

    added: mobile components

---
+++ app/(dashboard)/_components/NavbarRoutes.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+
+type Props = {};
+
+const NavbarRoutes = (props: Props) => {
+  return (
+    <div className="flex gap-x-2 ml-auto">
+      <UserButton />
+    </div>
+  );
+};
+
+export default NavbarRoutes;
