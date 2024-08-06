ede1f29b693e678936f756467dd934b012a817a6
4hmedhabib <info.ahmed.developer@gmail.com>
2024-08-06 14:52:03 +0300

    added: new componetns

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -5,6 +5,7 @@ import { LogOut } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
 import { usePathname, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import SearchInput from "./search-input";
 
 type Props = {};
 
@@ -14,29 +15,37 @@ const NavbarRoutes = (props: Props) => {
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isPlayerPage = pathname?.startsWith("/chapter");
+  const isSearchPage = pathname === "/search";
 
   return (
-    <div className="flex gap-x-2 ml-auto">
-      {isTeacherPage || isPlayerPage ? (
-        <Link href="/">
-          <Button
-            size="sm"
-            variant="ghost"
-            className="hover:bg-yellow-500 hover:text-white"
-          >
-            <LogOut className="h-4 w-4 mr-2" />
-            Exit
-          </Button>
-        </Link>
-      ) : (
-        <Link href="/teacher/courses">
-          <Button size="sm" variant="ghost">
-            Teacher Mode
-          </Button>
-        </Link>
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
       )}
-      <UserButton afterSignOutUrl="/" />
-    </div>
+      <div className="flex gap-x-2 ml-auto">
+        {isTeacherPage || isPlayerPage ? (
+          <Link href="/">
+            <Button
+              size="sm"
+              variant="ghost"
+              className="hover:bg-yellow-500 hover:text-white"
+            >
+              <LogOut className="h-4 w-4 mr-2" />
+              Exit
+            </Button>
+          </Link>
+        ) : (
+          <Link href="/teacher/courses">
+            <Button size="sm" variant="ghost">
+              Teacher Mode
+            </Button>
+          </Link>
+        )}
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </>
   );
 };
 
