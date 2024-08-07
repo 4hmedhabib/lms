b4cabc64e6e9b28f21fd180e518ddc5b8a238771
4hmedhabib <info.ahmed.developer@gmail.com>
2024-08-07 15:37:06 +0300

    added: courses component

---
+++ components/course-card.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import Link from "next/link";
+import IconBadge from "./icon-badge";
+import { BookOpen } from "lucide-react";
+import { formatPrice } from "@/lib/format";
+
+type Props = {
+  id: string;
+  title: string;
+  imageUrl: string | null;
+  price: number | null;
+  progress: number | null;
+  category?: string;
+  chaptersLength: number;
+};
+
+const CourseCard = ({
+  id,
+  title,
+  imageUrl,
+  price,
+  progress,
+  category,
+  chaptersLength
+}: Props) => {
+  return (
+    <Link href={`/courses/${id}`}>
+      <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
+        <div className="relative w-full aspect-video rounded-md overflow-hidden">
+          <Image
+            fill
+            className="object-cover"
+            alt={title}
+            src={imageUrl || ""}
+          />
+        </div>
+        <div className={"flex flex-col pt-2"}>
+          <div
+            className={
+              "text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2"
+            }
+          >
+            {title}
+          </div>
+          <p className="text-xs text-muted-foreground">{category}</p>
+          <div className="my-3 flex items-center gap-z-2 text-sm md:text-xs">
+            <div className="flex items-center gap-x-1 text-slate-500">
+              <IconBadge size="sm" icon={BookOpen} />
+              <span>
+                {chaptersLength} {chaptersLength === 1 ? "Chapter" : "Chapters"}
+              </span>
+            </div>
+          </div>
+          {!!progress ? (
+            <div>Todo: Progress Component</div>
+          ) : (
+            <p className="text-md md:text-sm font-medium text-slate-700">
+              {formatPrice(price || 0)}
+            </p>
+          )}
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default CourseCard;
