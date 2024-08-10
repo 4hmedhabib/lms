8529f0629d0d384ce9950d8693df9599e00aa960
4hmedhabib <info.ahmed.developer@gmail.com>
2024-08-10 14:09:46 +0300

    updated: navbar

--- app/(dashboard)/_components/Navbar.tsx
+++ app/(dashboard)/_components/Navbar.tsx
@@ -1,13 +1,18 @@
+import { getIsTeacher } from "@/actions/get-is-teacher";
 import MobileSidebar from "./MobileSidebar";
 import NavbarRoutes from "@/components/navbar-routes";
+import { auth } from "@clerk/nextjs";
 
 type Props = {};
 
-const Navbar = (props: Props) => {
+const Navbar = async (props: Props) => {
+  const { userId } = auth();
+  const isTeacher = await getIsTeacher(userId);
+
   return (
     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
       <MobileSidebar />
-      <NavbarRoutes />
+      <NavbarRoutes isTeacher={isTeacher} />
     </div>
   );
 };
